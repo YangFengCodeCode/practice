17ac5a28f8d642fe149be76b209c2df0185ba16e
杨枫 <2867108693@qq.com>
2023-06-10 11:49:22 +0800

    practice

---
+++ practice_36/practice_36/practice_36.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	float m = 0;
+//	float max = 0;
+//	float min = 100;
+//	float sum = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%f", &m);
+//		sum = sum + m;
+//		if (m > max)
+//		{
+//			max = m;
+//		}
+//		if (m < min)
+//		{
+//			min = m;
+//		}
+//		
+//	}
+//
+//	printf("%.2f %.2f %.2f", max, min, sum/n);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char arr1[100];
+//	char arr2[100];
+//	scanf("%s", arr1);
+//	scanf("%s", arr2);
+//	if (strcmp(arr1, arr2) == 0)
+//	{
+//		printf("same");
+//	}
+//	else
+//		printf("different");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[11][11];
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d %d", &n, &m);
+//	int i = 0;
+//	int sum = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//			if (arr[i][j] > 0)
+//			{
+//				sum += arr[i][j];
+//			}
+//		}
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
