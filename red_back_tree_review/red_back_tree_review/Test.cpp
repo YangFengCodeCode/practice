f8b60630a07581039e2c233b8215e977056c63ef
杨枫 <2867108693@qq.com>
2024-10-23 22:41:14 +0800

    red_black_tree

---
+++ red_back_tree_review/red_back_tree_review/Test.cpp
@@ -0,0 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+using namespace std;
+#include<iostream>
+#include"Tree.h"
---
+++ red_back_tree_review/red_back_tree_review/Tree.h
@@ -0,0 +1,366 @@
+﻿#pragma once
+
+//构建红黑树节点
+enum Colour
+{
+	RED,
+	BLACK
+};
+
+template<class K, class V>
+struct RBTreeNode
+{
+	RBTreeNode<K, V>* _left;
+	RBTreeNode<K, V>* _right;
+	RBTreeNode<K, V>* _parent;
+	pair<K, V> _kv;
+	Colour _col;
+
+	RBTreeNode(const pair<K, V>& kv)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _kv(kv)
+		, _col(RED)
+	{}
+};
+
+
+template<class K, class V>
+class RBTree
+{
+	typedef RBTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			_root->_col = BLACK;
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv); // 红色的
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		cur->_parent = parent;
+
+		//旋转
+		while (parent && parent->_col == RED)
+		{
+			Node* grandfather = parent->_parent;
+			if (parent == grandfather->_left)
+			{
+				Node* uncle = grandfather->_right;
+				// 情况一：叔叔存在且为红
+				if (uncle && uncle->_col == RED)
+				{
+					// 变色
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+
+					// 继续往上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else
+				{
+					// 情况二：叔叔不存在或者存在且为黑
+					// 旋转+变色
+					if (cur == parent->_left)
+					{
+						//       g
+						//    p    u
+						// c
+						RotateR(grandfather);
+						parent->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else
+					{
+						//       g
+						//    p     u
+						//      c
+						RotateL(parent);
+						RotateR(grandfather);
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}
+
+					break;
+				}
+			}
+
+			else
+			{
+				Node* uncle = grandfather->_left;
+				// 情况一：叔叔存在且为红
+				if (uncle && uncle->_col == RED)
+				{
+					// 变色
+					parent->_col = uncle->_col = BLACK;
+					grandfather->_col = RED;
+
+					// 继续往上处理
+					cur = grandfather;
+					parent = cur->_parent;
+				}
+				else
+				{
+					// 情况二：叔叔不存在或者存在且为黑
+					// 旋转+变色
+					//      g
+					//   u     p
+					//            c
+					if (cur == parent->_right)
+					{
+						RotateL(grandfather);
+						parent->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					else
+					{
+						//            g
+						//   u     p
+						//      c
+						RotateR(parent);
+						RotateL(grandfather);
+						cur->_col = BLACK;
+						grandfather->_col = RED;
+					}
+					break;
+				}
+			}
+		}
+
+		_root->_col = BLACK;//根节点是黑色
+		return true;
+	}
+
+
+
+	//左旋操作
+	void RotateL(Node* parent)
+	{
+		++rotateSize;
+
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+
+		parent->_right = subRL;
+		if (subRL)
+			subRL->_parent = parent;
+
+		subR->_left = parent;
+		Node* ppnode = parent->_parent;
+		parent->_parent = subR;
+
+		if (parent == _root)
+		{
+			_root = subR;
+			subR->_parent = nullptr;
+		}
+		else
+		{
+			if (ppnode->_left == parent)
+			{
+				ppnode->_left = subR;
+			}
+			else
+			{
+				ppnode->_right = subR;
+			}
+			subR->_parent = ppnode;
+		}
+	}
+
+
+	//右旋操作
+	void RotateR(Node* parent)
+	{
+		++rotateSize;
+
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+
+		parent->_left = subLR;
+		if (subLR)
+			subLR->_parent = parent;
+
+		subL->_right = parent;
+
+		Node* ppnode = parent->_parent;
+		parent->_parent = subL;
+
+		if (parent == _root)
+		{
+			_root = subL;
+			subL->_parent = nullptr;
+		}
+		else
+		{
+			if (ppnode->_left == parent)
+			{
+				ppnode->_left = subL;
+			}
+			else
+			{
+				ppnode->_right = subL;
+			}
+			subL->_parent = ppnode;
+		}
+	}
+
+
+
+
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+
+		_InOrder(root->_left);
+		cout << root->_kv.first << endl;
+		_InOrder(root->_right);
+	}
+
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+
+
+	size_t Size()
+	{
+		return _Size(_root);
+	}
+
+	size_t _Size(Node* root)
+	{
+		if (root == NULL)
+			return 0;
+
+		return _Size(root->_left)
+			+ _Size(root->_right) + 1;
+	}
+
+	Node* Find(const K& key)
+	{
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < key)
+			{
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > key)
+			{
+				cur = cur->_left;
+			}
+			else
+			{
+				return cur;
+			}
+		}
+
+		return NULL;
+	}
+
+	int _Height(Node* root)
+	{
+		if (root == nullptr)
+			return 0;
+
+		int leftHeight = _Height(root->_left);
+		int rightHeight = _Height(root->_right);
+
+		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+	}
+
+	int Height()
+	{
+		return _Height(_root);
+	}
+
+
+	int GetRotateSize()
+	{
+		return rotateSize;//统计旋转次数
+	}
+
+
+	bool Check(Node* cur, int blackNum, int refBlackNum)
+	{
+		if (cur == nullptr)
+		{
+			if (refBlackNum != blackNum)
+			{
+				cout << "黑色节点的数量不相等" << endl;
+				return false;
+			}
+			return true;
+		}
+
+		if (cur->_col == RED && cur->_parent->_col == RED)
+		{
+			cout << cur->_kv.first << "存在连续的红色节点" << endl;
+			return false;
+		}
+
+		if (cur->_col == BLACK)
+			++blackNum;
+
+		return Check(cur->_left, blackNum, refBlackNum)
+			&& Check(cur->_right, blackNum, refBlackNum);
+	}
+
+	bool IsBalance()
+	{
+		if (_root && _root->_col == RED)
+			return false;
+
+		int refBlackNum = 0;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_col == BLACK)
+				refBlackNum++;
+
+			cur = cur->_left;
+		}
+
+		return Check(_root, 0, refBlackNum);
+	}
+
+
+
+private:
+	Node* _root = nullptr;
+	int rotateSize = 0;
+};
